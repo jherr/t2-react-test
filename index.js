c3f6f4186d6d2c2653078693eee3ad7c4152fc2d
Jack Herrington <jherr@pobox.com>
2018-12-09 15:04:16 -0800

    First commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+let tessel = null;
+try {
+  tessel = require('tessel');
+} catch(e) {
+}
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+let servoPin = null;
+if (tessel) {
+  tessel.pwmFrequency(50);
+  servoPin = tessel.port.A.pwm[0];
+}
+
+// 20ms per cycle
+// 0.5 - 2.5ms target durations
+// 0.5 / 20 = 0.025
+// 2.5 / 20 = 0.125
+
+app.post('/api/position', (req, res) => {
+  const cycle = 0.025 + ((req.body.position / 180.0) * 0.1);
+  if (servoPin) {
+    servoPin.pwmDutyCycle(cycle);
+  } else {
+    console.log(cycle);
+  }
+  res.json({ok: true});
+});
+
+app.use('/', express.static(__dirname + '/build/index.html'));
+app.use('/', express.static(__dirname + '/build')); 
+app.listen(tessel ? 80 : 8080);
+
+if (tessel) {
+  tessel.network.wifi.connection(function(error, settings) {
+    console.log(`Server running at http://${settings.ip}/`);
+  });
+} else {
+  console.log("Server running at http://127.0.0.1:8080/");
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Slider from 'react-rangeslider';
 import './App.css';
+import 'react-rangeslider/lib/index.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      position: 90,
+    };
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(position) {
+    this.setState({ position });
+    fetch('/api/position', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        position: this.state.position,
+      })
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Slider
+          min={0}
+          max={180}
+          value={this.state.position}
+          onChange={this.onChange}
+        />
       </div>
     );
   }
